379556c01c46ec097f6e8f19a9e6e3515cfaa11e
dubalao <ra116409@uem.br>
2025-11-19 19:27:31 -0300

    att yacc e tests

--- yacc.py
+++ yacc.py
@@ -1,7 +1,104 @@
-from lex import tokens
-import ply.yacc as yacc 
+# Alunos:                    RA:
+#       Eduardo Andrade      116409
+#       Joao Vitor Pereira   125910
+#       Rodrigo Kenji        123630
+
+# ANALISADOR LEXICO E SINTÁTICO
+
+# PARTE 1: ANALISADOR LÉXICO
+import ply.yacc as yacc
+import ply.lex as lex
 import sys
 import json
+
+reserved = {
+    'program': 'PROGRAM',
+    'var': 'VAR',
+    'procedure': 'PROCEDURE',
+    'function': 'FUNCTION',
+    'begin': 'BEGIN',
+    'end': 'END',
+    'if': 'IF',
+    'then': 'THEN',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'do': 'DO',
+    'read': 'READ',
+    'write': 'WRITE',
+    'div': 'DIV',
+    'or': 'OR',
+    'and': 'AND',
+    'not': 'NOT',
+    'true': 'TRUE',
+    'false': 'FALSE',
+    'integer': 'TIPO_INTEIRO',
+    'boolean': 'TIPO_BOLEANO',
+}
+
+tokens = [
+    'IDENTIFICADOR',
+    'NUMERO',
+    'MAIS', 'MENOS', 'VEZES', 'ATRIB',
+    'IGUAL', 'DIF', 'MENOR', 'MENORIG', 'MAIOR', 'MAIORIG',
+    'PARE', 'PARD', 'PONTOV', 'PONTO', 'VIRG', 'DOISP'
+] + list(reserved.values())
+
+t_MAIS      = r'\+'
+t_MENOS     = r'-'
+t_VEZES     = r'\*'
+t_ATRIB     = r':='
+t_IGUAL     = r'='
+t_DIF       = r'<>'
+t_MENOR     = r'<'
+t_MENORIG   = r'<='
+t_MAIOR     = r'>'
+t_MAIORIG   = r'>='
+t_PARE      = r'\('
+t_PARD      = r'\)'
+t_PONTOV    = r';'
+t_PONTO     = r'\.'
+t_VIRG      = r','
+t_DOISP     = r':'
+
+def t_COMMENT(t):
+    r'(\{(.|\n)*?\})|(\(\*(.|\n)*?\*\))'
+    t.lexer.lineno += t.value.count('\n')
+    pass 
+
+def t_IDENTIFICADOR_INVALIDO(t):
+    r'\d+[a-zA-Z][a-zA-Z_0-9]*'
+    print(f"!!!Erro Léxico na Linha {t.lexer.lineno}: Identificador inválido '{t.value}' não pode começar com um número.!!!")
+    t.lexer.skip(len(t.value))
+
+def t_IDENTIFICADOR(t):
+    r'[a-zA-Z][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'IDENTIFICADOR')
+    return t
+
+def t_NUMERO_FLOAT(t):
+    r'(\d+\.\d+|\.\d+|\d+\.)'
+    print(f"!!!Erro Léxico na Linha {t.lexer.lineno}: Número real '{t.value}' não é permitido.!!!")
+    t.lexer.skip(len(t.value))
+
+def t_NUMERO(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore = ' \t'
+
+def t_error(t):
+    print(f"\nCaractere inválido '{t.value[0]}' encontrado na linha {t.lexer.lineno}\n")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+
+# PARTE 2: ANALISADOR SINTÁTICO
 
 precedence = (
     ('left', 'OR'),
